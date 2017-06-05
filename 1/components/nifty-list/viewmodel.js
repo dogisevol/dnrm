c7e36826a6de27d710c53e395f4acd51315147d7
dog.is.evol@gmail.com <dog.is.evol@gmail.com>
2017-06-05 12:01:45 +1000

    unused project removed

--- 1/components/nifty-list/viewmodel.js
+++
@@ -1,9 +0,0 @@
-define(['knockout'], function(ko) {
-
-    function NiftyListViewModel(params) {
-        this.componentText = params.componentText;
-    }
-    console.debug('component loaded');
-
-    return NiftyListViewModel;
-});
--- 1/components/viewmodel/nifty-list-item-viewmodel.js
+++
@@ -1,9 +0,0 @@
-define(['knockout'], function(ko) {
-
-    function NiftyListItemViewModel(params) {
-        this.labelText = params.labelText;
-    }
-    console.debug('component loaded');
-
-    return NiftyListItemViewModel;
-});
